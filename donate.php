1ee5fcc3b5f87e180c3c9ae687aa9f03d9ecc170
ainuldamia <50438476+ainuldamia@users.noreply.github.com>
2019-06-26 16:33:39 +0800

    Add files via upload

---
+++ donate.php
@@ -0,0 +1,111 @@
+<?php 
+// Initialize the session
+session_start();
+
+// // Check if the user is already logged in, if yes then redirect him to welcome page
+// if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+//     header("location: welcome.php");
+//     exit;
+// }
+
+// Include config file
+require_once "config.php";
+
+// Define variables and initialize with empty values
+$amount = $_POST[];
+
+$sql = "INSERT INTO donation (amount) VALUES ('$amount')";
+
+if ($link->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $link->error;
+}
+
+$link->close();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>SM Vision &mdash; Education Charity Organization</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700|Anton" rel="stylesheet">
+    
+
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
+
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/style.css">
+    
+  </head>
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+  
+  <div class="site-wrap"  id="home-section">
+
+    <div class="site-mobile-menu site-navbar-target">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div>
+   
+      
+      <?php include 'header.php'; ?>
+    
+
+    <div class="site-section bg-image overlay counter" style="background-image: url('images/hero_1_no-text.jpg');" id="about-section">
+    	<div class="container col-md-4 justify-content-center">
+    		<div class=" text-black">
+    			<h2>DONATE</h2>
+    			<p>Your donation help our work</p>
+    			<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+    				<label>RM</label>
+    				<input type="button" name="amount50" class="btn btn-primary" value="50">
+    				<input type="button" name="amount100" class="btn btn-primary" value="100">
+    				<input type="button" name="amount250" class="btn btn-primary" value="250">
+    				<input type="button" name="amount500" class="btn btn-primary" value="500">
+    				<br></br>
+    				<label>Other Amount: RM </label>
+    				<input type="number" name="amount" value="<?php echo $amount; ?>">
+    				<br>
+					<input type="submit" class="btn btn-primary" value="Donate">
+				</form>
+    		</div>
+    	</div>
+    </div>
+
+<?php include 'footer.php'; ?>
+
+  </div>
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/jquery.sticky.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+    
+  </body>
+</html>
